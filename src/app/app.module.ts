3b23debe4d00ba2317fa9211d24f4d0f1248db62
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-02-19 13:19:42 -0400

    iniciando ToDoListAngular projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToDoListModule } from './to-do-list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ToDoListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/to-do-list/to-do-list.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToDoListComponent } from './to-do-list-component';
+
+
+
+@NgModule({
+  declarations: [
+    ToDoListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ToDoListComponent
+  ]
+})
+export class ToDoListModule { }
