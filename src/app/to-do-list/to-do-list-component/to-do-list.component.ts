73e285575d961b5608edd6985860f5bd1acf3c91
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-03-20 02:19:05 -0400

    adicionando a logica de add task e a logica dos filtros

--- src/app/to-do-list/to-do-list-component/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list-component/to-do-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 import { ToDoListService } from '../to-do-list-service';
 
 @Component({
@@ -8,7 +8,11 @@ import { ToDoListService } from '../to-do-list-service';
 })
 export class ToDoListComponent implements OnInit {
   public ToDoList: ToDoListService
-  public TaskList: string[] = [];
+  public TaskList: string[] = ['Viajar de avião', 'Regar as plantas de manhã', 'Ir na academia'];
+  public qtdTask: number = this.TaskList.length;
+
+  @ViewChildren('liElement') liElement: QueryList<'liElement'>;
+  @ViewChildren('inputElement') inputElement: QueryList<'inputElement'>;
 
   constructor(public _ToDoList: ToDoListService) {
     this.ToDoList = _ToDoList;
@@ -22,9 +26,28 @@ export class ToDoListComponent implements OnInit {
     if (e.code === 'Enter' && (<HTMLInputElement>e.target).value.length > 0) {
       this.TaskList.unshift((<HTMLInputElement>e.target).value);
       (<HTMLInputElement>e.target).value = '';
+
+      this.itemsLeft(1);
     }
   }
 
+  removeTask(task: string): void {
+    this.TaskList.splice(this.TaskList.indexOf(task), 1);
+  }
 
+  itemsLeft(qtdItems: number): void {
+    this.qtdTask += qtdItems;
+  }
 
+  all(): void {
+    this.liElement.toArray().forEach((liElement: any) => {
+      if (liElement.nativeElement.classList.contains('esconder')) {
+        liElement.nativeElement.classList.remove('esconder');
+      }
+    });
+  }
+
+  active(): void {
+
+  }
 }
