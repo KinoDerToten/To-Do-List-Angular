9e9ad10809af594efeebf69852eb2b04cefa406e
Rodrigo Silva <rodrigodamasceno115@gmail.com>
2022-03-20 00:03:20 -0400

    adicionando o metodo para adicionar as tasks

--- src/app/to-do-list/to-do-list-component/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list-component/to-do-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChildren } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToDoListService } from '../to-do-list-service';
 
 @Component({
@@ -7,114 +7,24 @@ import { ToDoListService } from '../to-do-list-service';
   styleUrls: ['./to-do-list.component.css']
 })
 export class ToDoListComponent implements OnInit {
-  public tasks: object[] = [{
-    name: 'rodrigo',
-    id: 'silva'
-  }];
-  public qtdTask: number = 0;
-  public checkbox: boolean = true;
+  public ToDoList: ToDoListService
+  public TaskList: string[] = [];
 
-  @ViewChildren('checkElement') checkElement: any;
-  @ViewChildren('taskItem') taskItem: any;
-  @ViewChildren('liElement') liElement: any;
-  @ViewChildren('iconCheck') iconCheck: any;
-
-  constructor(public ToDoList: ToDoListService) {
+  constructor(public _ToDoList: ToDoListService) {
+    this.ToDoList = _ToDoList;
   }
 
   ngOnInit(): void {
 
   }
 
-  pressEnter(event: KeyboardEvent): void {
-    if (event.key === 'Enter') {
-      this.tasks = this.ToDoList.pressEnter(event);
-      this.qtdTask = this.tasks.length;
-      this.checkElement.toArray().forEach((checkElement: any) => {
-        if (checkElement.nativeElement.classList.contains('checked')) {
-          this.qtdTask -= 1;
-        }
-      });
+  addItemTask(e: KeyboardEvent): void {
+    if (e.code === 'Enter' && (<HTMLInputElement>e.target).value.length > 0) {
+      this.TaskList.unshift((<HTMLInputElement>e.target).value);
+      (<HTMLInputElement>e.target).value = '';
     }
   }
 
-  checked(id: string): void {
-    this.checkElement.toArray().forEach((checkElement: any) => {
-      if (checkElement.nativeElement.id == id) {
-        checkElement.nativeElement.classList.toggle('checkbox');
-        checkElement.nativeElement.classList.toggle('checked');
-        checkElement.nativeElement.firstChild.classList.toggle('icon-check');
 
-        if (checkElement.nativeElement.classList.contains('checked')) {
-          this.qtdTask -= 1;
-        } else {
-          this.qtdTask += 1;
-        }
-      }
-    });
-
-    this.liElement.toArray().forEach((liElement: any) => {
-      if (liElement.nativeElement.id == id) {
-        liElement.nativeElement.classList.toggle('checked-text')
-      }
-    });
-  }
-
-  completed(): void {
-    this.liElement.toArray().forEach((liElement: any) => {
-      if (liElement.nativeElement.classList.contains('checked-text')) {
-        liElement.nativeElement.classList.remove('esconder');
-        liElement.nativeElement.classList.add('mostrar');
-      } else {
-        liElement.nativeElement.classList.remove('mostrar');
-        liElement.nativeElement.classList.add('esconder');
-      }
-    });
-
-    this.qtdTask = 0;
-  }
-
-  active(): void {
-    this.qtdTask = 0;
-    this.liElement.toArray().forEach((liElement: any) => {
-      if (liElement.nativeElement.classList.contains('checked-text')) {
-        liElement.nativeElement.classList.remove('mostrar');
-        liElement.nativeElement.classList.add('esconder');
-      } else {
-        liElement.nativeElement.classList.remove('esconder');
-        liElement.nativeElement.classList.add('mostrar');
-        if (liElement.nativeElement.classList.contains('mostrar')) {
-          this.qtdTask += 1;
-        }
-      }
-    });
-  }
-
-  all(): void {
-    this.qtdTask = this.tasks.length;
-    this.checkElement.toArray().forEach((checkElement: any) => {
-      if (checkElement.nativeElement.classList.contains('checked')) {
-        this.qtdTask -= 1;
-      }
-    });
-    this.liElement.toArray().forEach((liElement: any) => {
-      if (liElement.nativeElement.classList.contains('esconder')) {
-        liElement.nativeElement.classList.remove('esconder');
-        liElement.nativeElement.classList.add('mostrar');
-      }
-    });
-  }
-
-  clearCompleted(): void {
-    this.liElement.toArray().forEach((liElement: any) => {
-      if (liElement.nativeElement.classList.contains('checked-text')) {
-        this.tasks.forEach((tasks: any) => {
-          if (tasks.name == liElement.nativeElement.id) {
-            this.tasks.splice(this.tasks.indexOf(tasks), 1);
-          }
-        });
-      }
-    });
-  }
 
 }
--- src/app/to-do-list/to-do-list-service/to-do-list-service.service.ts
+++ src/app/to-do-list/to-do-list-service/to-do-list-service.service.ts
@@ -4,18 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ToDoListService {
-  public tasks = [];
+
 
   constructor() { }
 
-  pressEnter(event: KeyboardEvent): object[] {
 
-    this.tasks.unshift({
-      name: (<HTMLInputElement>event.target).value,
-      id: (<HTMLInputElement>event.target).value
-    });
-    (<HTMLInputElement>event.target).value = '';
-
-    return this.tasks
-  }
 }
